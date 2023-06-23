31c6ce120d183dc04ad797740d94c6b7ba5e1400
Yuri Castilho <yuricastilho2005@gmail.com>
2023-06-22 23:25:48 -0300

    upload styles completed

--- screens/upload/uploadStyles.tsx
+++ screens/upload/uploadStyles.tsx
@@ -3,29 +3,48 @@ import { StyleSheet } from 'react-native';
 const uploadStyles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
   },
   container_upload: {
-
+    width: 260,
+    height: 260,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#eaebed',
+    borderStyle: 'dashed',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    padding: 16,
+    marginTop: 40,
   },
   upload_text: {
-
+    color: 'grey'
   },
   btn_upload: {
-    
+    height: 46,
+    marginTop: 14,
+    width: 260,
+    borderRadius: 4,
+    backgroundColor: '#35b471',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   btn_text: {
-    
+    color: 'white',
   },
     container_bytes: {
-    
+    marginTop: 100,
+    textAlign: 'center',
+    gap: 6,
   },
     bytes_percent: {
-    
+    fontSize: 32,
+    color: 'grey',
   },
     bytes_text: {
-    
+    color: 'grey',
+
   },
 });
 
