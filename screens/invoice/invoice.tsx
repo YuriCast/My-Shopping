d092850aa83e9b17c171689dcce84fecab960712
Yuri Castilho <yuricastilho2005@gmail.com>
2023-06-22 23:59:35 -0300

    invoice styles completed, less crud

--- screens/invoice/invoice.tsx
+++ screens/invoice/invoice.tsx
@@ -1,11 +1,34 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './invoiceStyles';
 
 function InvoiceScreen() {
   return (
     <View style={styles.container}>
-      <Text>Invoice!</Text>
+      <View style={styles.container_upload}>
+        <Text style={styles.upload_text}>Clique para adicionar ou para trocar a foto</Text>
+      </View>
+      <Text style={styles.info_foto}>
+        Informações da foto
+      </Text>
+      <View style={styles.crud}>
+        <View style={styles.crud_container}>
+          <Text style={styles.crud_title}>
+            Café
+          </Text>
+          <Text style={styles.crud_qtd}>
+           Quantidade: 3
+          </Text>
+          <View style={styles.crud_input}>
+          <TouchableOpacity 
+            style={styles.inputComplete}
+          />
+          <TouchableOpacity 
+            style={styles.inputDelete}
+          />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
--- screens/invoice/invoiceStyles.tsx
+++ screens/invoice/invoiceStyles.tsx
@@ -3,9 +3,58 @@ import { StyleSheet } from 'react-native';
 const invoiceStyles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-  }
-});
+  },
+  container_upload: {
+    width: 260,
+    height: 260,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#eaebed',
+    borderStyle: 'dashed',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    padding: 16,
+    marginTop: 40,
+  },
+  upload_text: {
+    color: 'grey'
+  },
+  info_foto: {
+
+  },
+  crud: {
+    marginTop: 50,
+    gap: 20,
+  },
+  crud_container: {
+    height: 92,
+    width: 312,
+    backgroundColor: '#eaebed',
+    justifyContent: 'center',
+    padding: 16
+  },
+  crud_title: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  crud_qtd: {
+    fontSize: 10
+  },
+  crud_input: {
+    
+  },
+  inputComplete: {
+    height: 30,
+    width: 30,
+    backgroundColor: '#35b471'
+  },
+  inputDelete: {
+    height: 30,
+    width: 30,
+    backgroundColor: 'red'
+
+  },});
 
 export default invoiceStyles;
