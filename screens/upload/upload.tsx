921afb8f6a54e8a3d42ec7ed0ae661a2e9547628
Yuri Castilho <yuricastilho2005@gmail.com>
2023-06-22 23:07:53 -0300

    added upload structure

--- screens/upload/upload.tsx
+++ screens/upload/upload.tsx
@@ -1,11 +1,26 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import styles from './uploadStyles';
 
 function uploadScreen() {
   return (
     <View style={styles.container}>
-      <Text>Upload!</Text>
+      <View style={styles.container_upload}>
+        <Text style={styles.upload_text}>Clique para adicionar ou para trocar a foto</Text>
+      </View>
+      <TouchableOpacity style={styles.btn_upload}>
+        <Text style={styles.btn_text}>
+          Fazer Upload
+        </Text>
+      </TouchableOpacity>
+      <View style={styles.container_bytes}>
+        <Text style={styles.bytes_percent}>
+          0%
+        </Text>
+        <Text style={styles.bytes_text}>
+          0 de 100 bytes transferidos
+        </Text>
+      </View>
     </View>
   );
 }
--- screens/upload/uploadStyles.tsx
+++ screens/upload/uploadStyles.tsx
@@ -5,7 +5,28 @@ const uploadStyles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  container_upload: {
+
+  },
+  upload_text: {
+
+  },
+  btn_upload: {
+    
+  },
+  btn_text: {
+    
+  },
+    container_bytes: {
+    
+  },
+    bytes_percent: {
+    
+  },
+    bytes_text: {
+    
+  },
 });
 
 export default uploadStyles;
